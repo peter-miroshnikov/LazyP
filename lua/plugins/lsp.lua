1088996eb1caf454daaa507138ff789a53f276ed
Peter Miroshnikov <peter.miroshnikov@gmail.com>
2025-02-03 09:58:36 +0200

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,6 @@
 return {
+  { "gpanders/editorconfig.nvim" },
+
   -- add pyright to lspconfig
   {
     "neovim/nvim-lspconfig",
@@ -149,7 +151,35 @@ return {
     version = false, -- set this if you want to always pull the latest change
     opts = {
       provider = "copilot",
-      -- add any opts here
+      --provider = "ollama",
+      --use_absolute_path = true,
+      --vendors = {
+      --  ---@type AvanteProvider
+      --  ollama = {
+      --    ["local"] = true,
+      --    endpoint = "127.0.0.1:11434/v1",
+      --    model = "codegemma",
+      --    parse_curl_args = function(opts, code_opts)
+      --      return {
+      --        url = opts.endpoint .. "/chat/completions",
+      --        headers = {
+      --          ["Accept"] = "application/json",
+      --          ["Content-Type"] = "application/json",
+      --        },
+      --        body = {
+      --          model = opts.model,
+      --          messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+      --          max_tokens = 2048,
+      --          stream = true,
+      --        },
+      --      }
+      --    end,
+      --    parse_response_data = function(data_stream, event_state, opts)
+      --      require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+      --    end,
+      --  },
+      --},
+      --  add any opts here
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
